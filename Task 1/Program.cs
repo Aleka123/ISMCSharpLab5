391d3879aaa7447e4b70637117a77de2bacf17b1
Aleka123 <rainbowray@ukr.net>
2016-09-19 18:26:48 +0300

    Lab5

---
+++ Task 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите х");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y");
+            double y = double.Parse(Console.ReadLine());
+            double z = 0, g = 0, c = 0, a = 0, b = 0;
+            a = x * x;
+            b = Math.Sqrt(x * x + 10);
+            c = (y + (1 / Math.Sqrt(x * x + 10)));
+            if (c != 0 && b != 0 && a != 0)
+            {
+                {
+                    z = ((x + (2 + y) / a) / c);
+                    g = 2.8 * Math.Sin(x) + Math.Abs(y);
+                    Console.Write("z=" + z);
+                    Console.Write("g=" + g);
+                }
+            }
+            else
+                Console.WriteLine("нету решения, знаменатель равняеться 0");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите а");
+            double a = double.Parse(Console.ReadLine());
+            double a4, a6, a7, a8, a9, a10,a2;
+            a2 = a * a;
+
+            a4=a*a;
+            a4 *= a4;
+
+            a6 = a4*a2;
+
+            
+            a7 = a2*a4*a;
+
+            a8 =;
+
+            a10 =a4*a4;
+            Console.WriteLine(a);
+        }
+    }
+}
