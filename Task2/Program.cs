be9c70a40929aa6f19dadef6eb155b8959770d77
Олександра Луцевич <rainbowray@ukr.net>
2016-09-21 17:05:26 +0300

    Add files via upload

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -12,21 +12,41 @@ namespace Task2
         {
             Console.WriteLine("Введите а");
             double a = double.Parse(Console.ReadLine());
-            double a4, a6, a7, a8, a9, a10,a2;
-            a2 = a * a;
-
-            a4=a*a;
-            a4 *= a4;
-
-            a6 = a4*a2;
-
-            
-            a7 = a2*a4*a;
-
-            a8 =;
-
-            a10 =a4*a4;
-            Console.WriteLine(a);
+            double b,c,d,e,f,k,l,m,n,w,y;
+            b = a * a;
+            c = b * b;
+            Console.Write("a4=");
+            Console.WriteLine(c);
+            d = b * c;
+            Console.Write("a6=");
+            Console.WriteLine(d);
+            e = d * a;
+            Console.Write("a7=");
+            Console.WriteLine(e);
+            f = c * c;
+            Console.Write("a8=");
+            Console.WriteLine(f);
+            e = f * a;
+            Console.Write("a9=");
+            Console.WriteLine(e);
+            k = e * a;
+            Console.Write("a10=");
+            Console.WriteLine(k);
+            l = k * b * a;
+            Console.Write("a13=");
+            Console.WriteLine(l);
+            m = l * b;
+            Console.Write("a15=");
+            Console.WriteLine(m);
+            n = m *d ;
+            Console.Write("a21=");
+            Console.WriteLine(n);
+            w = n * e;
+            Console.Write("a28=");
+            Console.WriteLine(w);
+            y = w * w * f;
+            Console.Write("a64=");
+            Console.WriteLine(y);
         }
     }
 }
