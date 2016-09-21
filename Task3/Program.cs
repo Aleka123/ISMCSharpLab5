1e94474b5ad9ffcef3a01cd53f6a036adf750bd4
Олександра Луцевич <rainbowray@ukr.net>
2016-09-21 17:05:51 +0300

    Add files via upload

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Console.WriteLine("Введите  трехзначное число a=");
+            float a = float.Parse(Console.ReadLine());
+            double b,c,d,e,f;
+            b =a%10;
+            c = (a%100-b)/10;
+            e = (a - 10*c - b )/100;
+            d =b+c+e;
+            f =b*c*e;
+                Console.WriteLine("количество единиц="+b);
+            Console.WriteLine("количество десятков=" + c);
+            Console.WriteLine("сумма цифр=" + d);
+            Console.WriteLine("произведение его цифр=" + f);
+        }
+    }
+}
